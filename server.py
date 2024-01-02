86baab5a91a2bc1f33c5adf4c3bc324a648bdc73
digital-installer <155440232+digital-installer@users.noreply.github.com>
2024-01-02 10:58:28 +0000

    New python server.py
    New file added and fixed

---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+import  json
+
+app = Flask("My Hello World Application")
+
+products = [
+    {'id': 143, 'name': 'Notebook', 'price': 5.49},
+    {'id': 144, 'name': 'Black Marker', 'price': 1.99}
+]
+
+@app.route("/")
+def hello():
+    return {"name":"Hello World!"}
+
+# Example request - http://localhost:5000/products
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+# Example request - http://localhost:5000/products/144 - with method GET
+@app.route('/products/<id>', methods=['GET'])
+def get_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    return jsonify(product)
+
+# Example request - http://localhost:5000/products - with method POST
+@app.route('/products', methods=['POST'])
+def add_product():
+    products.append(request.get_json())
+    return '', 201
+
+# Example request - http://localhost:5000/products/144 - with method PUT
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+    id = int(id)
+    updated_product = json.loads(request.data)
+    product = [x for x in products if x["id"] == id][0]
+    for key, value in updated_product.items():
+        product[key] = value
+    return '', 204
+
+# Example request - http://localhost:5000/products/144 - with method DELETE
+@app.route('/products/<id>', methods=['DELETE'])
+def remove_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    products.remove(product)
+    return '', 204
+
+if __name__=="__main__":
+    app.run(debug=True)
+# When no port is specified, starts at default port 5000
